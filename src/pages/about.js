865b529d43ea1b03f232c39098c3111ae6fd9a59
Taylor McPherson <taylor@lifeblue.com>
2021-12-30 15:28:35 -0800

    Temp about.

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import ProjectCards from '../components/projectCards'
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <div className="About">
+          <h1>Taylor McPherson is a Front End Engineer and User Advocate.</h1>
+      </div>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
