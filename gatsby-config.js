cd506d34397acd881a19b19206c3fe22b72c3d81
Taylor McPherson <taylor@lifeblue.com>
2022-01-10 12:48:38 -0800

    height update.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
